b00a677e12d9bd82485ba66fc27df69e540b11e3
Stephen Hughes <Hughes0687@gmail.com>
2015-10-02 15:05:36 -0500

    added d3 and such

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,5 +1,13 @@
 class SurveyController < ApplicationController
   def index
-    @surveys = Survey.find_each
+    @surveys = Survey.find_each.to_json
+  end
+
+  def show
+    respond_to do |format|
+      format.json {
+        render :json => Survey.find_each.to_json
+      }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'survey#index'
   resources :survey
+  get 'survey/data', :defaults => { :format => 'json' }
 end
