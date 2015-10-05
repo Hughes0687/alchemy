0899ae5e94a1afbe2f822736e08db69fc94aa458
Stephen Hughes <Hughes0687@gmail.com>
2015-10-05 12:19:57 -0500

    got some simple d3js working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,7 +24,8 @@ $.ajax({
   url: 'survey/data',
   dataType: 'json',
   success: function (data) {
-    draw(data);
+    array(data);
+    console.log(data);
     window.data = data;
   },
   error: function (result) {
@@ -33,52 +34,34 @@ $.ajax({
 });
 
 
-function draw(data) {
-
+function array(data) {
+  window.array = [];
   $.each( data, function( key, value ) {
-    //alert( value.comment );
+    window.array.push(value.question_one);
+    //alert( value.question_one );
   });
-
+  draw(window.array)
 }
 
-//function draw(data) {
-//
-//  var color = d3.scale.category20b();
-//  var width = 420,
-//    barHeight = 20;
-//
-//  var x = d3.scale.linear()
-//    .range([0, width])
-//    .domain([0, d3.max(data)]);
-//
-//  var chart = d3.select("#graph")
-//    .attr("width", width)
-//    .attr("height", barHeight * data.length);
-//
-//  var bar = chart.selectAll("g")
-//    .data(data)
-//    .enter().append("g")
-//    .attr("transform", function (d, i) {
-//      return "translate(0," + i * barHeight + ")";
-//    });
-//
-//  bar.append("rect")
-//    .attr("width", x)
-//    .attr("height", barHeight - 1)
-//    .style("fill", function (d) {
-//      return color(d)
-//    });
-//
-//  bar.append("text")
-//    .attr("x", function (d) {
-//      return x(d) - 10;
-//    })
-//    .attr("y", barHeight / 2)
-//    .attr("dy", ".35em")
-//    .style("fill", "white")
-//    .text(function (d) {
-//      return d;
-//    });
-//}
+function draw(data) {
 
-$(function(){ $(document).foundation(); });
+  var x = d3.scale.linear()
+    .domain([0, 5])
+    .range([0, 420]);
+
+  d3.select(".chart")
+    .selectAll("div")
+    .data(data)
+    .enter().append("div")
+    .style("width", function (d) {
+      return x(d) + "px";
+    })
+    .text(function (d) {
+      return d;
+    });
+
+
+  $(function () {
+    $(document).foundation();
+  });
+}
