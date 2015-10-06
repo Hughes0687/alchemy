868a2326312a515480b86363a55d67d3c089bd26
Stephen Hughes <Hughes0687@gmail.com>
2015-10-06 11:06:40 -0500

    fixed naming

--- db/migrate/20151002212246_add_columns_to_survey.rb
+++ db/migrate/20151002212246_add_columns_to_survey.rb
@@ -1,7 +1,7 @@
 class AddColumnsToSurvey < ActiveRecord::Migration
   def change
-    add_column :survey, :user_name, :string
-    add_column :survey, :employer_id, :integer
-    add_column :survey, :created_at, :datetime
+    add_column :surveys, :user_name, :string
+    add_column :surveys, :employer_id, :integer
+    add_column :surveys, :created_at, :datetime
   end
 end
